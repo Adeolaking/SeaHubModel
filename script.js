0c2967d68e2c1f54d64439a8e0c292357bc545fa
Adeolaking <adeolakinga@gmail.com>
2022-04-28 08:21:19 +0100

    development

---
+++ script.js
@@ -0,0 +1,14 @@
+const productList = document.querySelector('.products-list');
+
+
+for (let i = 0; i < 20; i++) productList.append = <li class="product">
+<div class="product-image">
+    <img src="./media/clean.jpg"
+    width="100%" alt=""/>
+</div>
+<div class="product-discription">
+    <h2>Pencile</h2>
+    <p>$20</p>
+</div>
+<div class="btn">Add to cart</div>
+</li>
